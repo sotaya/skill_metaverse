99d3dab423109938049484c3c4d570076e430fda
SOTAKANA\Sota1 <Sota1248R@outlook.jp>
2025-10-17 22:49:15 +0900

    Revision01

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDE_S1cSnBIYgE0ktxiUtV0RmzOXunYB68",
-  authDomain: "skill-metaverse-3f991.firebaseapp.com",
-  projectId: "skill-metaverse-3f991",
-  storageBucket: "skill-metaverse-3f991.firebasestorage.app",
-  messagingSenderId: "307465756414",
-  appId: "1:307465756414:web:32c11bf970548a7f974faf",
-  measurementId: "G-1FB77SFXT9",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "",
 };
 
 const app = initializeApp(firebaseConfig);
